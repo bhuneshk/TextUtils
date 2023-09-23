952790d46c12bfd4557b2c93d411514b18d09cf3
bhuneshk <kumarbhunesh3@gmail.com>
2023-09-23 22:44:41 +0530

    More Functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar'
-import TextForm from './components/TextForm'
+// import TextForm from './components/TextForm'
 
 
 function App() {
   return (
     <>
      <Navbar/>
-     <TextForm heading='Enter the text to analyze'/>
+     {/* <TextForm heading='Enter the text to analyze'/> */}
+     <About/>
      </>
+  
   );
 }
 
