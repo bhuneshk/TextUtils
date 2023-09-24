e2fdcae0abc67dc20eb7782ac1adb9dafd4581ee
bhuneshk <kumarbhunesh3@gmail.com>
2023-09-24 23:52:31 +0530

    deployment error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import PropTypes from 'prop-types'
-import TextForm from './TextForm'
+// import TextForm from './TextForm'
 // import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
